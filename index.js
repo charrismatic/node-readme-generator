3a2847b2f11bc52096a0b0bcc539e357f70a20f1
Matt Harris <m@ttharr.is>
2019-06-04 20:15:40 -0400

    updated cli command and tested running with command flags /inputs

--- index.js
+++ index.js
@@ -1,22 +1,57 @@
-const fs = require('fs')
+#!/usr/bin/env node
+
+const fs = require('fs');
 const f = require('./src/formatters');
 const schema = require('./src/package_json_default');
+var options;
+
+// PROCESS ENV
+const parse_options = () => {
+  var defaults = {
+    input: false,
+    debug: false,
+    user_cwd: false,
+    out_file: 'README.md',
+    dry_run: false
+  };
+
+  var runtime_options = process.env.READMEGEN_ENV;
+  if (runtime_options) {
+    console.log('With options', runtime_options );
+  } else {
+    runtime_options = '{}';
+  }
+
+  var _options = Object.assign({}, defaults, JSON.parse(runtime_options));
+
+  if (_options.debug) {
+    console.log('process env', process.env.READMEGEN_ENV );
+    console.log('current options', _options );
+  }
+
+  return _options;
+};
 
 if (process.env.npm_package_version){
   var my_pkg = process.env.npm_package_version;
 } else {
+  options = parse_options();
+  if (options.debug) {
+    console.log('program options', options);
+  }
+
+  var base_dir = options.user_cwd;
   try {
-      if (fs.existsSync('./package.json')) {
-        my_pkg = require('./package');
+    if (fs.existsSync(`${base_dir}/package.json`)) {
+      my_pkg = require(`${base_dir}/package`);
     } else {
-      console.error("No package.json file could be found");
+      console.error('No package.json file could be found');
       process.exit(0);
     }
   } catch(err) {
-   console.error(err)
+    console.error(err);
   }
 }
-
 
 
 // AVOID TYPE ERRORS FROM MISSING VALUES IN THE USER PACKAGE.JSON
@@ -26,39 +61,41 @@ var readme = [];
 // CREATE THE README OBJECT
 readme.push(f.h1(pkg.name));
 readme.push(f.blockquot(pkg.description ));
-readme.push(f.key_value("Version", pkg.version),f.br);
+readme.push(f.key_value('Version', pkg.version),f.br);
 readme.push(f.br);
 readme.push('Topics: ');
-for (word of pkg.keywords){
+for (var word of pkg.keywords){
   readme.push(f.link(word, `https://github.com/topics/${word}`));
   readme.push(',  ');
 }
 readme.push(f.br);
 readme.push(f.br);
-readme.push(f.h2("About"));
+readme.push(f.h2('About'));
 readme.push(f.link(pkg.homepage, pkg.homepage), f.br);
+readme.push(f.h3('Author'));
 readme.push(f.obj_to_list(pkg.author));
 readme.push(f.br,f.br,f.line,f.br);
-readme.push(f.h3("Project Repo"));
+readme.push(f.h3('Project Repo'));
 readme.push(f.obj_to_list(pkg.repository),f.br);
-readme.push(f.h2("Dependencies"));
+readme.push(f.h2('Dependencies'));
 readme.push(f.obj_to_list(pkg.dependencies),f.br);
-readme.push(f.h3("Config Options"));
+readme.push(f.h3('Config Options'));
 readme.push(f.obj_to_list(pkg.config),f.br);
-readme.push(f.h2("Usage"));
+readme.push(f.h2('Usage'));
 readme.push(f.obj_to_list(pkg.scripts),f.br);
-readme.push(f.h2("Development"));
+readme.push(f.h2('Development'));
 readme.push(f.obj_to_list(pkg.devDependencies),f.br);
-readme.push(f.h3("Contributors"));
+readme.push(f.h3('Contributors'));
 readme.push(f.obj_to_list(pkg.contributors),f.br);
-readme.push(f.h3("Issues"));
+readme.push(f.h3('Issues'));
 readme.push(f.obj_to_list(pkg.bugs),f.br);
 readme.push(f.br);
 readme.push(f.line);
 readme.push(f.br);
-readme.push(f.h3("Licesnse"));
+readme.push(f.h3('Licesnse'));
 readme.push(pkg.license);
 readme.push(f.link(pkg.license, `https://opensource.org/licenses/${pkg.licesnse}`));
+readme.push('\n\n');
 
 
 const readme_data = readme.join('');
@@ -70,17 +107,24 @@ try {
     readme_file = 'PACKAGE_README.md';
   }
 } catch(err) {
-  console.error(err)
+  console.error(err);
 }
 
 // WRITE THE FILE
 fs.open(readme_file, 'wx', (err, fd) => {
   if (err) {
     if (err.code === 'EEXIST') {
-      console.error(`${readme_file} already exists`);
-      return;
+      if (options.overwrite_existing){
+        console.log('Notice: README file exists... ');
+        console.log('Command ran with  "--force" flag, continuing file save.');
+        // continue
+      } else {
+        console.log('README data', readme_data);
+        console.error(`${readme_file} already exists`);
+        return;
+        throw err;
+      }
     }
-    throw err;
   }
   fs.writeFileSync(readme_file, readme_data);
 });
--- src/formatters.js
+++ src/formatters.js
@@ -1,16 +1,24 @@
 module.exports = {
-
   line: '---',
   br: '\n',
-  h1: (txt) => { return "\n" + "# " + txt + "\n\n"; },
-  h2: (txt) => { return "\n" + "## " + txt + "\n\n"; },
-  h3: (txt) => { return "\n" + "### " + txt + "\n\n"; },
-  h4: (txt) => { return "\n" + "#### " + txt + "\n\n"; },
-  h5: (txt) => { return "\n" + "##### " + txt + "\n\n"; },
-  h6: (txt) => { return "\n" + "###### " + txt + "\n\n"; },
-  blockquot: (txt) => { return "\n" + "> " + txt + "\n\n"; },
-  key_value: (key, value) => { return key + ": " + value;   },
-  link: (text, href) => { return `[${text}](${href})` },
+  h1: (txt) => { return '\n' + '# ' + txt + '\n\n'; },
+  h2: (txt) => { return '\n' + '## ' + txt + '\n\n'; },
+  h3: (txt) => { return '\n' + '### ' + txt + '\n\n'; },
+  h4: (txt) => { return '\n' + '#### ' + txt + '\n\n'; },
+  h5: (txt) => { return '\n' + '##### ' + txt + '\n\n'; },
+  h6: (txt) => { return '\n' + '###### ' + txt + '\n\n'; },
+  blockquot: (txt) => { return '\n' + '> ' + txt + '\n\n'; },
+  key_value: (key, value) => { return key + ': ' + value; },
+  link: (text, href) => { return `[${text}](${href})`; },
+  clean_html_whitespace: (html) => {
+    return html.replace(/>[\s|\n|\t]+</g,'><').replace(/\s/g,' ').trim();
+  },
+  clean_text_whitespace: (text) => {
+    return text.replace(/\s/g,' ').replace(/\t/g,'  ').replace(/\n+/g,'\n\n').trim();
+  },
+  clean_url: (url) => {
+    return url.replace(/http[s]?\:\/\/(www)?[\.]?/,'');
+  },
   create_output_header: function (url, title ) {
     let header = '\n\n';
     header += url + '\n\n';
@@ -18,65 +26,52 @@ module.exports = {
     header += '---------------------------\n';
     return header;
   },
-  clean_html_whitespace: (html) => {
-    return html.replace(/\>[\s|\n|\t]+\</g,'><').replace(/ /g,' ').trim();
-  },
-  clean_text_whitespace: (text) => {
-    return text.replace(/ /g,' ').replace(/\t/g,'  ').replace(/\n+/g,'\n\n').trim();
-  },
-  clean_url: (url) => {
-    return url.replace(/http[s]?\:\/\/(www)?[\.]?/,'');
-  },
   obj_to_list: (obj) => {
-    let result_arr = [];
-    for (item of Object.entries(obj)) {
-      result_arr.push(`-  ${item[0]}: ` + JSON.stringify(item[1])) 
+    if(typeof obj === 'object'){
+      let result_arr = [];
+      for (var item of Object.entries(obj)) {
+        console.log(typeof item[0]);
+        result_arr.push(`-  ${item[0]}: ` + JSON.stringify(item[1]));
+      }
+      return result_arr.join('\n');
     }
-    return result_arr.join('\n')
+    else if(typeof obj === 'string'){
+      return obj;
+    } else {
+      return JSON.stringify(obj);
+    }
   },
   arr_to_list: (arr) => {
     let result_arr = [];
-    arr.forEach(function(item){ result_arr.push('- ' + item) });
-    return result_arr.join('\n')
+    arr.forEach(function(item){ result_arr.push('- ' + item); } );
+    return result_arr.join('\n');
   },
   render_form_data: (forms) => {
     var form_result = [];
     var result = [];
-    result.push("\n---\n");
-    for (form of forms){
-      form_result = parse_node_properties( form, 'attributes' );
-      result.push(form_result.trim());
-      var inputs = form.querySelectorAll('input');
-      var input_results = ['  '];
-      for ( input of inputs ){
-        input_row = parse_node_properties( input , 'attributes');
-        input_results.push( "  " + input_row );
-      }
-      result.push( input_results.join('\n') );
-      result.push("---\n");
-    }
+    result.push('\n---\n');
     var header = '### SITE FORMS \n\n';
     return (header + result.join('\n').trim());
   },
   doc_headers: (data) => {
-    var header = "\n\n";
-    header += href + "\n\n";
-    header += "# " +  document.title + "\n\n";
-    header += "---------------------------\n";
+    var header = '\n\n';
+    header += href + '\n\n';
+    header += '# ' +  document.title + '\n\n';
+    header += '---------------------------\n';
     return header;
   },
   get_site_meta_tags: (() => {
     var result = [];
-    meta = document.querySelectorAll('head meta');
+    var meta = document.querySelectorAll('head meta');
     meta.forEach( function( node ){
       var row = [];
       var attrs = node.attributes;
       for ( var attr of attrs ) {
-        row.push("" + attr.name + "__" +  attr.value + "");
+        row.push('' + attr.name + '__' +  attr.value + '');
       }
-      result.push("-  " + row.join(" || ") + '\n' );
+      result.push('-  ' + row.join(' || ') + '\n' );
     } );
-    header = '### SITE META TAGS';
-    return  "\n\n" + header + "\n\n " + result.join('\n') + "\n\n";
+    var header = '### SITE META TAGS';
+    return  '\n\n' + header + '\n\n ' + result.join('\n') + '\n\n';
   })
 }
